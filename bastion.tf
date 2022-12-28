e0583272551d2f584a4cc8420aa9d2fb97f127b2
santospv <paulovitor8@gmail.com>
2022-12-28 01:12:12 -0300

    add bastion host

---
+++ bastion.tf
@@ -0,0 +1,24 @@
+resource "google_compute_address" "static" {
+  name = "ipv4-address"
+}
+resource "google_compute_instance" "bastion_host" {
+  name = sensitive("${var.project_id}-bastion")
+  project = var.project_id
+  machine_type = "e2-medium"
+  zone = var.zone
+  tags = ["gce", sensitive("${var.project_id}-bastion")]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      size = "30"
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.terraform-network.name
+    subnetwork = google_compute_subnetwork.subnet.name
+    access_config {
+    }
+  }
+}
